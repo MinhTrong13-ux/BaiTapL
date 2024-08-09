f6937534ddc2a700541b94ae75538bad794c890c
MinhTrong13-ux <danh51340@gmail.com>
2024-08-09 22:15:48 +0700

    vs8

---
+++ baiTapLon.js
@@ -0,0 +1,10 @@
+window.onload=function()
+{
+    let main= document.getElementById("main");
+    let images = document.querySelectorAll(".thumb img");
+    for(let im of images)
+    im.onclick=function()
+    {
+        main.src = this.src;
+    }
+}
