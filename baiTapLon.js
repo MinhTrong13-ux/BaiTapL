8ff367e43c342f71291611710eb7fe8b94f70c37
MinhTrong13-ux <danh51340@gmail.com>
2024-08-11 23:52:07 +0700

    vs9

--- baiTapLon.js
+++ baiTapLon.js
@@ -8,3 +8,19 @@ window.onload=function()
         main.src = this.src;
     }
 }
+$(document).ready(function() {
+    // Hiển thị nút khi cuộn xuống
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 10) { // Hiển thị nút khi cuộn xuống hơn 100px
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    // Cuộn mượt mà về đầu trang khi nhấp vào nút
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800); // 800ms cho hiệu ứng cuộn mượt mà
+        return false; // Ngăn không làm mới trang
+    });
+})
