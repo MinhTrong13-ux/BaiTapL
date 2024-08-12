91b15be2e0175218d54c266f7af293a740e3d94a
MinhTrong13-ux <danh51340@gmail.com>
2024-08-12 22:19:06 +0700

    vs11

--- baiTapLon.js
+++ baiTapLon.js
@@ -2,22 +2,20 @@ window.onload=function()
 {
     let main= document.getElementById("main");
     let images = document.querySelectorAll(".thumb img");
-    let menu = document.getElementsByClassName("menu");
     for(let im of images)
     im.onclick=function()
     {
         main.src = this.src;
     }
-// window.onscroll = function()
-// {
-// if(this.scrolly >= menu.offsetTop){
-//     menu.style.position = 'fixed';
-//     menu.style.top = 0;
-//     menu.style.left = 0;
-//     menu.style.right = 0;
-//     menu.style.zIndex=999;
-// }
-// }
+     let menu = document.getElementById('menu');
+    window.onscroll = function(){
+        if(menu.scrollY >= menu.offsetTop){
+            menu.style.position = 'fixed';
+            menu.style.top=0;
+        }else{
+            menu.style.position = 'initial';
+        }
+    }
 }
 $(document).ready(function() {
     $(window).scroll(function() {
@@ -27,18 +25,18 @@ $(document).ready(function() {
             $('.back-to-top').fadeOut();
         }
     });
-})
     $('.back-to-top').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 800);
         return false;
     });
-//     $(document).ready(function()
-// {
-//     $('.car').hover(function()
-// {
-//     $('#btbuy').fadeIn(300)},
-//     function()
-//     { 
-//         $('#btbuy').fadeOut(300)}
-// );
-// });
+})
+    $(document).ready(function()
+{
+    $('.car').hover(function()
+{
+    $('#btbuy').fadeIn(300)},
+    function()
+    { 
+        $('#btbuy').fadeOut(300)
+    });
+})
