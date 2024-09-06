97d1a6ea01b9aca39f64e241a6847777968a407f
MinhTrong13-ux <danh51340@gmail.com>
2024-09-06 20:19:43 +0700

    vs2

--- baiTapLon.js
+++ baiTapLon.js
@@ -67,19 +67,21 @@ window.onload=function()
         };
     };
         let process = document.getElementById("addProcess");
-        let productsDiv = document.querySelector(".products");
         process.onclick = function(){
             let namekg = document.getElementById("namekg").value;
             let giakg = document.getElementById("giakg").value;
             let anhkg = document.getElementById("anhkg").value;
-            let h = `<div class="product">
+            let h = `
+                    <div class="product">
                         <div>
                             <img src="${anhkg}" alt="Xe ky gui">
                             <p>${namekg}</p>
                             <div>${giakg}<sup>vnd</sup></div>  
                             </div>
-                    </div>`
-             productsDiv.innerHTML = h + productsDiv.innerHTML;
+                    </div>
+                    `
+                 let first = document.querySelector(".product:first-child");
+                 first.insertAdjacentHTML("beforebegin", h);
     };  
 };
 $(document).ready(function() {
